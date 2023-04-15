e089e4f51fc15266a13c3c608fb54c8b96d5adf3
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2023-04-15 13:05:39 -0400

    replaced tabs as spaces

--- Add_Cover_to_Video.py
+++ Add_Cover_to_Video.py
@@ -19,80 +19,80 @@ def cls():
 
 def cover_video(droppedFile, outputPath, cover, container):
 
-	ffmpeg_cmd = FFMPEG_PATH
-	ffmpeg_cmd += ' -y'
-	ffmpeg_cmd += f' -i "{droppedFile}"'
+    ffmpeg_cmd = FFMPEG_PATH
+    ffmpeg_cmd += ' -y'
+    ffmpeg_cmd += f' -i "{droppedFile}"'
 
-	if container == "mp4":
-		ffmpeg_cmd += f' -i "{cover}"'
+    if container == "mp4":
+        ffmpeg_cmd += f' -i "{cover}"'
 
-		ffmpeg_cmd += ' -map 0 -map 1'
-		ffmpeg_cmd += ' -c copy'
-		ffmpeg_cmd += ' -c:v:1 mjpeg'
-		ffmpeg_cmd += ' -disposition:v:1 attached_pic'
+        ffmpeg_cmd += ' -map 0 -map 1'
+        ffmpeg_cmd += ' -c copy'
+        ffmpeg_cmd += ' -c:v:1 mjpeg'
+        ffmpeg_cmd += ' -disposition:v:1 attached_pic'
 
-	if container == "mkv":
-		ffmpeg_cmd += f' -attach "{cover}"'
-		ffmpeg_cmd += ' -metadata:s:t mimetype=image/jpeg'
-		ffmpeg_cmd += ' -c copy'
-		ffmpeg_cmd += ' -map 0'
+    if container == "mkv":
+        ffmpeg_cmd += f' -attach "{cover}"'
+        ffmpeg_cmd += ' -metadata:s:t mimetype=image/jpeg'
+        ffmpeg_cmd += ' -c copy'
+        ffmpeg_cmd += ' -map 0'
 
-	ffmpeg_cmd += f' "{outputPath}"'
+    ffmpeg_cmd += f' "{outputPath}"'
 
-	print(ffmpeg_cmd)
-	subprocess.call(ffmpeg_cmd)
+    print(ffmpeg_cmd)
+    subprocess.call(ffmpeg_cmd)
 
 if __name__ == "__main__":
 
-	### Checking FFmpeg installation
+    ### Checking FFmpeg installation
 
-	FFMPEG_PATH = shutil.which('ffmpegu') or './ffmpeg/ffmpeg'
-	try:
-	    subprocess.run([FFMPEG_PATH, '-version'], check=True)  # ensure ffmpeg is available
-	except:
-	    print(f'{cmdUtils.Red}Error: Couldn\'t find FFMPEG.{cmdUtils.Grey}')
-	    input()
-	    sys.exit(1)
+    FFMPEG_PATH = shutil.which('ffmpegu') or './ffmpeg/ffmpeg'
+    try:
+        subprocess.run([FFMPEG_PATH, '-version'], check=True)  # ensure ffmpeg is available
+    except:
+        print(f'{cmdUtils.Red}Error: Couldn\'t find FFMPEG.{cmdUtils.Grey}')
+        input()
+        sys.exit(1)
 
-	### Get dragndroped file and add file output suffix
-	droppedFile = sys.argv[1]
-	print(f'{cmdUtils.Green}Detected input file : {droppedFile}{cmdUtils.Grey}')
+    ### Get dragndroped file and add file output suffix
+    droppedFile = sys.argv[1]
+    print(f'{cmdUtils.Green}Detected input file : {droppedFile}{cmdUtils.Grey}')
 
-	# droppedFile = "./testSubjects/sequence/throwhands%04d.jpg"
-	outputPath = f'{droppedFile.rsplit(".",1)[0]}__{time.strftime("%Y-%m-%d_%H-%M-%S")}.{droppedFile.rsplit(".",1)[1]}'
-	print(f'{cmdUtils.Green}Output file will be: {outputPath}{cmdUtils.Grey}')
+    # droppedFile = "./testSubjects/sequence/throwhands%04d.jpg"
+    outputPath = f'{droppedFile.rsplit(".",1)[0]}__{time.strftime("%Y-%m-%d_%H-%M-%S")}.{droppedFile.rsplit(".",1)[1]}'
+    print(f'{cmdUtils.Green}Output file will be: {outputPath}{cmdUtils.Grey}')
 
-	### CONTAINER CHECK ###
+    ### CONTAINER CHECK ###
 
-	def get_container(file):
-		_container = droppedFile.rsplit(".",1)[1]
-		if _container.lower() == "mp4":
-			return "mp4"
-		if _container.lower() == "mkv":
-			return "mkv"
+    def get_container(file):
+        _container = droppedFile.rsplit(".",1)[1]
+        if _container.lower() == "mp4":
+            return "mp4"
+        if _container.lower() == "mkv":
+            return "mkv"
 
-		else:
-			print(f'{cmdUtils.Red}Error: Only MP4 and MKV files are supported for now.{cmdUtils.Grey}')
-			sys.exit(1)
+        else:
+            print(f'{cmdUtils.Red}Error: Only MP4 and MKV files are supported for now.{cmdUtils.Grey}')
+            sys.exit(1)
 
-	### COVER SETUP ###
-	def get_cover(file):
-		_cover = os.path.dirname(file) + "/cover.jpg"
-		print(f'{cmdUtils.Tan}Checking if {_cover} exists...{cmdUtils.Grey}')
-		if os.path.exists(_cover) == True:
-			return _cover
-		else:
-			print(f'{cmdUtils.Red}Error: Couldn\'t find "cover.jpg" alongside input video file.{cmdUtils.Grey}')
-			sys.exit(1)
+    ### COVER SETUP ###
+    def get_cover(file):
+        _cover = os.path.dirname(file) + "/cover.jpg"
+        print(f'{cmdUtils.Tan}Checking if {_cover} exists...{cmdUtils.Grey}')
+        if os.path.exists(_cover) == True:
+            return _cover
+        else:
+            print(f'{cmdUtils.Red}Error: Couldn\'t find "cover.jpg" alongside input video file.{cmdUtils.Grey}')
+            sys.exit(1)
 
-	cover = get_cover(droppedFile)
-	print(f'{cmdUtils.Green}Found cover file: {cover}{cmdUtils.Grey}')
-	container = get_container(droppedFile)
-	print(f'{cmdUtils.Green}Detected container: {container}{cmdUtils.Grey}')
+    cover = get_cover(droppedFile)
+    print(f'{cmdUtils.Green}Found cover file: {cover}{cmdUtils.Grey}')
+    container = get_container(droppedFile)
+    print(f'{cmdUtils.Green}Detected container: {container}{cmdUtils.Grey}')
 
-	### Run main fun
-	cover_video(droppedFile, outputPath, cover, container)
+    ### Run main fun
+    cover_video(droppedFile, outputPath, cover, container)
 
-	print(cmdUtils.Sound)
-	print(f'{cmdUtils.Green}Successfully created {outputPath}\nThis window will close after 10 seconds.{cmdUtils.Grey}')
-	subprocess.call('timeout /t 10')
+    print(cmdUtils.Sound)
+    print(f'{cmdUtils.Green}Successfully created {outputPath}\nThis window will close after 10 seconds.{cmdUtils.Grey}')
+    subprocess.call('timeout /t 10')
--- Video_to_MP4-H264.py
+++ Video_to_MP4-H264.py
@@ -19,47 +19,47 @@ def cls():
 
 def encode_video(droppedFile, outputPath, framerate, crf=21, preset="ultrafast"):
 
-	ffmpeg_cmd = FFMPEG_PATH
-	ffmpeg_cmd += ' -y'
-	ffmpeg_cmd += f' -i "{droppedFile}"'
-	ffmpeg_cmd += f' -r {framerate}'
+    ffmpeg_cmd = FFMPEG_PATH
+    ffmpeg_cmd += ' -y'
+    ffmpeg_cmd += f' -i "{droppedFile}"'
+    ffmpeg_cmd += f' -r {framerate}'
 
-	ffmpeg_cmd += f' -c:v libx264 -crf {crf} -preset {preset}'
-	ffmpeg_cmd += ' -c:a aac'
+    ffmpeg_cmd += f' -c:v libx264 -crf {crf} -preset {preset}'
+    ffmpeg_cmd += ' -c:a aac'
 
-	ffmpeg_cmd += f' "{outputPath}"'
+    ffmpeg_cmd += f' "{outputPath}"'
 
-	print(ffmpeg_cmd)
-	subprocess.call(ffmpeg_cmd)
+    print(ffmpeg_cmd)
+    subprocess.call(ffmpeg_cmd)
 
 if __name__ == "__main__":
 
-	### Checking FFmpeg installation
+    ### Checking FFmpeg installation
 
-	FFMPEG_PATH = shutil.which('ffmpeg') or './ffmpeg/ffmpeg'
-	try:
-	    subprocess.run([FFMPEG_PATH, '-version'], check=True)  # ensure ffmpeg is available
-	except:
-	    print(f'{cmdUtils.Red}Error: Couldn\'t find FFMPEG.{cmdUtils.Grey}')
-	    input()
-	    sys.exit(1)
+    FFMPEG_PATH = shutil.which('ffmpeg') or './ffmpeg/ffmpeg'
+    try:
+        subprocess.run([FFMPEG_PATH, '-version'], check=True)  # ensure ffmpeg is available
+    except:
+        print(f'{cmdUtils.Red}Error: Couldn\'t find FFMPEG.{cmdUtils.Grey}')
+        input()
+        sys.exit(1)
 
-	### FRAMERATE SETUP ###
-	cls()
-	framerate = input("Type the desired Framerate:")
-	if framerate == "": framerate = 24
-	# framerate = 24
+    ### FRAMERATE SETUP ###
+    cls()
+    framerate = input("Type the desired Framerate:")
+    if framerate == "": framerate = 24
+    # framerate = 24
 
-	### Get dragndroped file and add file output suffix
-	droppedFile = sys.argv[1]
-	print(f'{cmdUtils.Green}Detected input file : {droppedFile}{cmdUtils.Grey}')
+    ### Get dragndroped file and add file output suffix
+    droppedFile = sys.argv[1]
+    print(f'{cmdUtils.Green}Detected input file : {droppedFile}{cmdUtils.Grey}')
 
-	outputPath = f'{droppedFile.rsplit(".",1)[0]}__{time.strftime("%Y-%m-%d_%H-%M-%S")}.mp4'
-	print(f'{cmdUtils.Green}Output file will be: {outputPath}{cmdUtils.Grey}')
+    outputPath = f'{droppedFile.rsplit(".",1)[0]}__{time.strftime("%Y-%m-%d_%H-%M-%S")}.mp4'
+    print(f'{cmdUtils.Green}Output file will be: {outputPath}{cmdUtils.Grey}')
 
-	### Run main fun
-	encode_video(droppedFile, outputPath, framerate)
+    ### Run main fun
+    encode_video(droppedFile, outputPath, framerate)
 
-	print(cmdUtils.Sound)
-	print(f"{cmdUtils.Green}Encoding succesful. This window will close after 10 seconds.{cmdUtils.Grey}")
-	subprocess.call('timeout /t 10')
+    print(cmdUtils.Sound)
+    print(f"{cmdUtils.Green}Encoding succesful. This window will close after 10 seconds.{cmdUtils.Grey}")
+    subprocess.call('timeout /t 10')
